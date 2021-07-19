86425f7ef73d82224314d8501bd7fc37eb711fc7
jort5475 <87135876+jort5475@users.noreply.github.com>
2021-07-19 21:23:40 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and volume of Google!
+
+""")
+
+tickerSymbol = 'GOOGL'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf= tickerData.history(period='1d', start = '2010-5-31', end = '2020-5-31')
+st.line_chart(tickerDf.Open)
+st.line_chart(tickerDf.Volume)
